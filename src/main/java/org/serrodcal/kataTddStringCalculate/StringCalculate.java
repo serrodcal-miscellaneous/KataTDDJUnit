ef4b402a9b92f4e38b72345767d26cd48b8f851f
sergio.rodriguez <sergio.rodriguez@aptus.es>
2015-09-17 14:09:49 +0200

    First iteration, red

---
+++ src/main/java/org/serrodcal/kataTddStringCalculate/StringCalculate.java
@@ -0,0 +1,9 @@
+package org.serrodcal.kataTddStringCalculate;
+
+public class StringCalculate {
+
+	public int run(String args) {
+		return -1;
+	}
+    
+}
---
+++ src/test/java/org/serrodcal/kataTddStringCalculate/StringCalculateTest.java
@@ -0,0 +1,31 @@
+package org.serrodcal.kataTddStringCalculate;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class StringCalculateTest {
+
+	@Before
+    public void setUp() {
+        //TODO
+    }
+
+    @After
+    public void tearDown() {
+       //TODO
+    }
+    
+    @Test
+    public void emptyShouldReturnZero(){
+    	StringCalculate calculate = new StringCalculate();
+    	
+    	assertEquals(0, calculate.run(""));
+    }
+	
+}
