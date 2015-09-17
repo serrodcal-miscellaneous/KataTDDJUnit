bb17fdb573b370400ba787727f58908adb31a64a
sergio.rodriguez <sergio.rodriguez@aptus.es>
2015-09-17 14:47:47 +0200

    Fourth iteration, refactor

--- src/main/java/org/serrodcal/kataTddStringCalculate/StringCalculate.java
+++ src/main/java/org/serrodcal/kataTddStringCalculate/StringCalculate.java
@@ -2,17 +2,23 @@ package org.serrodcal.kataTddStringCalculate;
 
 public class StringCalculate {
 
+	private static final String DELIMITER = ",";
+
 	public int run(String args) {
 		if(args.isEmpty())
 			return 0;
-		else if(args.contains(",")){
-			String[] splits = args.split(",");
-			Integer sum = 0;
-			for(String num: splits)
-				sum += toInt(num);
-			return sum;
+		else if(args.contains(DELIMITER)){
+			String[] splits = args.split(DELIMITER);
+			return sumSplitsNumbers(splits);
 		}else
 			return toInt(args);
+	}
+
+	private Integer sumSplitsNumbers(String[] splits) {
+		Integer sum = 0;
+		for(String num: splits)
+			sum += toInt(num);
+		return sum;
 	}
 
 	private int toInt(String args) {
