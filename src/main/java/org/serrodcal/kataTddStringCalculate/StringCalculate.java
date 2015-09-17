5ed5b1baf8ed97c9bc0902e052ca588d8dbc225e
sergio.rodriguez <sergio.rodriguez@aptus.es>
2015-09-17 14:12:58 +0200

    Second iteration, green

--- src/main/java/org/serrodcal/kataTddStringCalculate/StringCalculate.java
+++ src/main/java/org/serrodcal/kataTddStringCalculate/StringCalculate.java
@@ -3,7 +3,10 @@ package org.serrodcal.kataTddStringCalculate;
 public class StringCalculate {
 
 	public int run(String args) {
-		return 0;
+		if(args.isEmpty())
+			return 0;
+		else
+			return Integer.parseInt(args);
 	}
     
 }
